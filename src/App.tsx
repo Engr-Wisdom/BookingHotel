77a9ea1400b76cd2f53bf20c5ef99fa5fef1124e
Wisdom Effiong <wisdomeffiong2008@gmail.com>
2026-09-22 15:32:20 +0100

    Added ScrollToTop component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
 
 const Home = lazy(() => import("./pages/Home"));
 const Hotels = lazy(() => import("./pages/Hotels"));
@@ -32,6 +33,8 @@ function App() {
         </div>
       }
     >
+      <ScrollToTop />
+      
       <Routes>
         <Route path="/" element={<Home />} />
 
